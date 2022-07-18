d89587164f7d6530023d95313d86b3d9b42256ae
Mariana Gallego <marianagallego97@hotmail.com>
2022-07-18 13:41:07 -0500

    Project uploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayComponent } from './components/display/display.component';
+import { InvoiceViewerComponent } from './views/invoice-viewer/invoice-viewer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+      component: DisplayComponent,
+      pathMatch: 'full'
+  },
+  {
+    path: 'viewFile',
+    component: InvoiceViewerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { DisplayComponent } from './components/display/display.component';
+import { InvoiceViewerComponent } from './views/invoice-viewer/invoice-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DisplayComponent,
+    InvoiceViewerComponent
   ],
   imports: [
     BrowserModule,
